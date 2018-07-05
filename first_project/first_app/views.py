297f5b20ef98b22712dc028b7d4178bf4b5e98e0
ohduran <alvaro.duranb@gmail.com>
2018-07-05 20:55:57 +0100

    Modularise templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -7,4 +7,4 @@ def index(request):
     my_dictionary = {
         'insert_me': "Hello, I am from views.py",
     }
-    return render(request, 'index.html', context=my_dictionary)
+    return render(request, 'first_app/index.html', context=my_dictionary)
