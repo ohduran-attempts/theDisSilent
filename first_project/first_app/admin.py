239ea20b13814005a44555e08ca5da3226a228e1
ohduran <alvaro.duranb@gmail.com>
2018-07-09 07:24:36 +0100

    admin complete

--- first_project/first_app/admin.py
+++ first_project/first_app/admin.py
@@ -4,3 +4,5 @@ from first_app.models import AccessRecord, Topic, Webpage
 admin.site.register(AccessRecord)
 admin.site.register(Topic)
 admin.site.register(Webpage)
+#djangoadmin
+ #alvaro@alvaro.com
