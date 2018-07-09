07e2e81869997d436d016ebdf7e600f94e9ff9d8
ohduran <alvaro.duranb@gmail.com>
2018-07-09 20:04:18 +0100

    New views and index.html

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, AddressRecord, Webpage
 # Create your views here.
 
 def index(request):
-    my_dictionary = {
-        'insert_me': "Hello, I am from views.py",
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {
+        'access_records': webpages_list,
     }
-    return render(request, 'first_app/index.html', context=my_dictionary)
+    return render(request, 'first_app/index.html', context=date_dict)
