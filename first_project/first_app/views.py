a42b130ea10de308799de77eaefc44cc4c0aee64
ohduran <alvaro.duranb@gmail.com>
2018-07-09 20:06:47 +0100

    Changes on views

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from first_app.models import Topic, AddressRecord, Webpage
+from first_app.models import Topic, AccessRecord, Webpage
 # Create your views here.
 
 def index(request):
