db78b924eb02176c7f7e47c23de3c0c94b389d66
ohduran <alvaro.duranb@gmail.com>
2018-07-09 07:46:12 +0100

    populating script

--- first_project/populate_first_app.py
+++ first_project/populate_first_app.py
@@ -18,3 +18,34 @@ topics = [
     'News',
     'Games',
 ]
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+def populate(N=5):
+
+    for entry in range(N):
+
+        # get the topic for the entry
+        top = add_topic()
+
+        # Create fake data
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        # Create the new webpage entry
+        webpage = Webpage.objects.get_or_create(
+            topic=top,
+            url=fake_url,
+            name=fake_name)[0]
+
+        # Create a fake access record for that webpage
+        accessrecord = AccessRecord.objects.get_or_create(name=webpage, date=fake_date)[0]
+
+if __name__ == '__main__':
+    print('populating script')
+    populate(20)
+    print('Populate complete')
