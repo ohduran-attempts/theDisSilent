ded331d8ad802bf5f9a1287c8a9feff5c957d4a8
ohduran <alvaro.duranb@gmail.com>
2018-07-11 19:59:03 +0100

    added hidden field to avoid scrapers

--- project_forms/basicapp/forms.py
+++ project_forms/basicapp/forms.py
@@ -5,3 +5,12 @@ class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,
+                                                                widget=forms.HiddenInput)  # input of type hidden
+
+    def clean_botcatcher(self):
+        botcatcher = self.cleaned_data['botcatcher']
+        # a human won't ever fill this value
+        if len(botcatcher) > 0:
+            raise forms.ValidationError("Gotcha, bot!")
+        return botcatcher
