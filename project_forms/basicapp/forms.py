e3a5f620406cd916fe2d7b446447c1c4d6384880
ohduran <alvaro.duranb@gmail.com>
2018-07-11 20:08:29 +0100

    Validation forms

--- project_forms/basicapp/forms.py
+++ project_forms/basicapp/forms.py
@@ -1,16 +1,27 @@
 from django import forms
+from django.core import validators
 
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label='Enter your email again')
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.CharField(required=False,
-                                                                widget=forms.HiddenInput)  # input of type hidden
+                                                                widget=forms.HiddenInput,
+                                                                validators=[validators.MaxLengthValidator(0)])  # input of type hidden, must be length 0
 
-    def clean_botcatcher(self):
-        botcatcher = self.cleaned_data['botcatcher']
-        # a human won't ever fill this value
-        if len(botcatcher) > 0:
-            raise forms.ValidationError("Gotcha, bot!")
-        return botcatcher
+    def clean(self):
+        all_clean_data = super().clean()
+        email = all_clean_data['email']
+        vmail = all_clean_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("Make sure emails match")
+
+    # def clean_botcatcher(self):
+    #     botcatcher = self.cleaned_data['botcatcher']
+    #     # a human won't ever fill this value
+    #     if len(botcatcher) > 0:
+    #         raise forms.ValidationError("Gotcha, bot!")
+    #     return botcatcher
