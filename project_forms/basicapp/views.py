e87eeb68237424859f05698ac6180b35e3f30d24
ohduran <alvaro.duranb@gmail.com>
2018-07-11 07:46:14 +0100

    Form views

--- project_forms/basicapp/views.py
+++ project_forms/basicapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from . import forms
 # Create your views here.
+
+def index(request):
+    return render(request, 'basicapp/index.html')
+
+def form_name_view(request):
+    form = form.FormName()
+    return render(request, 'basicapp/forms.html', {'form':form})
--- project_forms/project_forms/urls.py
+++ project_forms/project_forms/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from basicapp import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^formpage/', views.form_name_view, name='form_name'),
+
 ]
