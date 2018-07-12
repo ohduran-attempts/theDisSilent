bd08f33eaae8ace26fadf1bba9490b4229848807
ohduran <alvaro.duranb@gmail.com>
2018-07-12 07:27:34 +0100

    change forms and views

--- pro_two/apptwo/views.py
+++ pro_two/apptwo/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from apptwo.models import User
+#from django.http import HttpResponse
+#from apptwo.models import User
+from apptwo.forms import NewUserForm
 # Create your views here.
 
 def index(request):
     return render(request, 'apptwo/index.html')
 
 def users(request):
-    user_list = User.objects.order_by('first_name')
-    user_dict = {
-        'users': user_list,
-    }
-    return render(request, 'apptwo/users.html', context=user_dict)
+
+    form = NewUserForm()
+
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print('Invalid Form')
+
+    return render(request, 'apptwo/users.html', context={'form':form})
