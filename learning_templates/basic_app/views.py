9c93e612020e817b10ea95036e87751b5976edfa
ohduran <alvaro.duranb@gmail.com>
2018-07-15 11:41:35 +0100

    template filters

--- learning_templates/basic_app/views.py
+++ learning_templates/basic_app/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'basic_app/index.html')
+    context = {
+    'text':'Hello, world',
+    'number':101,
+    }
+    return render(request, 'basic_app/index.html', context)
 
 def other(request):
     return render(request, 'basic_app/other.html')
-    
+
 def relative(request):
     return render(request, 'basic_app/relative_url_template.html')
