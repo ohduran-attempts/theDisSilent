12e23a2fa9d325327b11d72d5b042f35588f888d
ohduran <alvaro.duranb@gmail.com>
2018-07-15 18:05:35 +0100

    forms and models for users

--- learning_users/basic_app/models.py
+++ learning_users/basic_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class UserProfileInfo(models.Model):
+
+    # don't inherit
+    user = models.OneToOneField(User)
+
+    # additional fields
+    portfolio_site = models. URLField(blank=True)
+
+    # save under media/profile_pics
+    profile_pic = models.ImageField(upload_to='profile_pics', blank=True)
+
+    def __str__(self):
+        return self.user.username
