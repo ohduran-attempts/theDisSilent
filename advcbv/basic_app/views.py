966794937c3c7cb67b5590e3fe49f85820bbaf23
ohduran <alvaro.duranb@gmail.com>
2018-07-19 18:44:55 +0100

    Class Based View hello world

--- advcbv/basic_app/views.py
+++ advcbv/basic_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+class CBView(View):
+    def get(self, request):
+        return HttpResponse("Class Based View!")
